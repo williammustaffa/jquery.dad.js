a67e41336b8d9f1cfe6d155cbf49a7838245e614
williammustaffa <william.lima.cat@gmail.com>
2020-07-23 02:28:12 -0300

    Start refactoring

--- demo/scripts/jquery.dad.js
+++ demo/scripts/jquery.dad.js
@@ -5,327 +5,264 @@
 
 (function ($) {
   "use strict";
+
   var supportsTouch = "ontouchstart" in window || navigator.msMaxTouchPoints;
 
-  $.fn.dad = function (opts) {
-    var _this = this;
+  /**
+   * Mouse constructor
+   */
+  function DadMouse() {
+    this.positionX = 0;
+    this.positionY = 0;
+    this.offsetX = 0;
+    this.offsetY = 0;
+  }
 
-    var defaults = {
-      target: ">div",
-      draggable: false,
-      placeholder: "",
-      callback: false,
-      containerClass: "dad-container",
-      childrenClass: "dads-children",
-      cloneClass: "dads-children-clone",
-      active: true,
-    };
+  /**
+   * Mouse udpate event
+   * @param {Event}
+   */
+  DadMouse.prototype.update = function (e) {
+    // Update mouse coordinates
+    this.positionX = e.pageX;
+    this.positionY = e.pageY;
+  };
 
-    var options = $.extend({}, defaults, opts);
+  var mouse = new DadMouse();
 
-    $(this).each(function () {
-      var active = options.active;
-      var $daddy = $(this);
-      var childrenClass = options.childrenClass;
-      var cloneClass = options.cloneClass;
-      var jQclass = "." + childrenClass;
-      var $target = $daddy.find(options.target);
-      var placeholder = options.placeholder;
-      var callback = options.callback;
-      var dragClass = "dad-draggable-area";
-      var holderClass = "dads-children-placeholder";
+  /**
+   * DAD class constructor
+   * @param {element} element
+   * @param {options} options
+   */
+  function Dad(element, options) {
+    this.options = options;
 
-      // HANDLE MOUSE
-      var mouse = {
-        x: 0,
-        y: 0,
-        target: false,
-        clone: false,
-        placeholder: false,
-        cloneoffset: {
-          x: 0,
-          y: 0,
-        },
-        updatePosition: function (e) {
-          this.x = e.pageX;
-          this.y = e.pageY;
-        },
+    // jQuery elements
+    this.$container = $(element);
+    this.$children = this.$container.children();
+    this.$target = null;
+    this.$clone = null;
 
-        move: function (e) {
-          this.updatePosition(e);
-          if (this.clone !== false && _this.target !== false) {
-            this.clone.css({
-              left: this.x - this.cloneoffset.x,
-              top: this.y - this.cloneoffset.y,
-            });
-          }
-        },
-      };
+    // General variables
+    this.mouse = new DadMouse();
+    this.active = true;
 
-      $(window).on("mousemove touchmove", function (e) {
-        var ev = e;
+    // Add event listeners, data attributes and etc
+    this.setup();
+  }
 
-        if (mouse.clone !== false && mouse.target !== false) e.preventDefault();
+  Dad.prototype.setup = function () {
+    // Add dad-id attribute to children
+    this.$children.each(function (index) {
+      var $this = $(this);
+      $this.data("dad-id", index);
+      $this.data("dad-position", index);
+    });
+    // Add element event listeners
+    this.$children.on("mousedown touchstart", this.start.bind(this));
+    // Add window event listeners
+    $(window).on("mousemove touchmove", this.update.bind(this));
+    $(window).on("mouseup touchend", this.end.bind(this));
+  };
 
-        if (supportsTouch && e.type == "touchmove") {
-          ev = e.originalEvent.touches[0];
-          var mouseTarget = document.elementFromPoint(ev.clientX, ev.clientY);
+  /**
+   * First step, occurs on mousedown
+   * @param {Event}
+   */
+  Dad.prototype.start = function (e) {
+    // TODO: draggable
+    // If already has a target, do nothing
+    if (this.$target) return;
 
-          $(mouseTarget).trigger("touchenter");
-        }
+    // Update "mouse" position for touch devices
+    if (e.type == "touchstart") {
+      this.mouse.updatePosition(e.originalEvent.touches[0]);
+    }
 
-        mouse.move(ev);
-      });
+    this.$target = $(e.target);
 
-      $daddy.addClass(options.containerClass);
+    // Add clone
+    this.$clone = this.$target.clone();
+    this.$container.append(this.$clone);
 
-      if (!$daddy.hasClass("dad-active") && active === true) {
-        $daddy.addClass("dad-active");
-      }
-
-      _this.addDropzone = function (selector, func) {
-        $(selector)
-          .on("mouseenter touchenter", function () {
-            if (mouse.target !== false) {
-              mouse.placeholder.css({ display: "none" });
-              mouse.target.css({ display: "none" });
-              $(this).addClass("active");
-            }
-          })
-          .on("mouseup touchend", function () {
-            if (mouse.target != false) {
-              mouse.placeholder.css({ display: "block" });
-              mouse.target.css({ display: "block" });
-              func(mouse.target);
-              dadEnd();
-            }
-
-            $(this).removeClass("active");
-          })
-          .on("mouseleave touchleave", function () {
-            if (mouse.target !== false) {
-              mouse.placeholder.css({ display: "block" });
-              mouse.target.css({ display: "block" });
-            }
-
-            $(this).removeClass("active");
-          });
-      };
-
-      // GET POSITION FUNCTION
-      _this.getPosition = function () {
-        var positionArray = [];
-        $(this)
-          .find(jQclass)
-          .each(function () {
-            positionArray[$(this).attr("data-dad-id")] = parseInt(
-              $(this).attr("data-dad-position")
-            );
-          });
-
-        return positionArray;
-      };
-
-      _this.activate = function () {
-        active = true;
-        if (!$daddy.hasClass("dad-active")) {
-          $daddy.addClass("dad-active");
-        }
-
-        return _this;
-      };
-
-      // DEACTIVATE FUNCTION
-      _this.deactivate = function () {
-        active = false;
-        $daddy.removeClass("dad-active");
-        return _this;
-      };
-
-      // DEFAULT DROPPING
-      $daddy.on("DOMNodeInserted", function (e) {
-        var $thisTarget = $(e.target);
-        if (
-          !$thisTarget.hasClass(childrenClass) &&
-          !$thisTarget.hasClass(holderClass)
-        ) {
-          $thisTarget.addClass(childrenClass);
-        }
-      });
-
-      $(document).on("mouseup touchend", function () {
-        dadEnd();
-      });
-
-      // ORDER ELEMENTS
-      var order = 1;
-      $target.addClass(childrenClass).each(function () {
-        if ($(this).data("dad-id") == undefined) {
-          $(this).attr("data-dad-id", order);
-        }
-
-        $(this).attr("data-dad-position", order);
-        order++;
-      });
-
-      // CREATE REORDER FUNCTION
-      function updatePosition(e) {
-        var order = 1;
-        e.find(jQclass).each(function () {
-          $(this).attr("data-dad-position", order);
-          order++;
-        });
-      }
-
-      // END EVENT
-      function dadEnd() {
-        if (mouse.target != false && mouse.clone != false) {
-          if (callback != false) {
-            callback(mouse.target);
-          }
-
-          var appear = mouse.target;
-          var desappear = mouse.clone;
-          var holder = mouse.placeholder;
-          var bLeft = 0;
-          var bTop = 0;
-
-          // Maybe we will use this in the future
-          //Math.floor(parseFloat($daddy.css('border-left-width')));
-          //Math.floor(parseFloat($daddy.css('border-top-width')));
-          if ($.contains($daddy[0], mouse.target[0])) {
-            mouse.clone.animate(
-              {
-                top: mouse.target.offset().top - $daddy.offset().top - bTop,
-                left: mouse.target.offset().left - $daddy.offset().left - bLeft,
-              },
-              300,
-              function () {
-                appear
-                  .css({
-                    visibility: "visible",
-                  })
-                  .removeClass("active");
-                desappear.remove();
-              }
-            );
-          } else {
-            mouse.clone.fadeOut(300, function () {
-              desappear.remove();
-            });
-          }
-
-          holder.remove();
-          mouse.clone = false;
-          mouse.placeholder = false;
-          mouse.target = false;
-          updatePosition($daddy);
-        }
-
-        $("html, body").removeClass("dad-noSelect");
-      }
-
-      // UPDATE EVENT
-      function dadUpdate(obj) {
-        if (mouse.target !== false && mouse.clone !== false) {
-          var $origin = $('<span style="display:none"></span>');
-          var $newplace = $('<span style="display:none"></span>');
-
-          if (obj.prevAll().hasClass("active")) {
-            obj.after($newplace);
-          } else {
-            obj.before($newplace);
-          }
-
-          mouse.target.before($origin);
-          $newplace.before(mouse.target);
-
-          // UPDATE PLACEHOLDER
-          mouse.placeholder.css({
-            top: mouse.target.offset().top - $daddy.offset().top,
-            left: mouse.target.offset().left - $daddy.offset().left,
-            width: mouse.target.outerWidth() - 10,
-            height: mouse.target.outerHeight() - 10,
-          });
-
-          $origin.remove();
-          $newplace.remove();
-        }
-      }
-
-      // GRABBING EVENT
-      var jq = options.draggable !== false ? options.draggable : jQclass;
-      $daddy.find(jq).addClass(dragClass);
-      $daddy.on("mousedown touchstart", jq, function (e) {
-        // For touchstart we must update "mouse" position
-        if (e.type == "touchstart") {
-          mouse.updatePosition(e.originalEvent.touches[0]);
-        }
-
-        if (
-          mouse.target == false &&
-          active == true &&
-          (e.which == 1 || e.type == "touchstart")
-        ) {
-          var $self = $(this);
-
-          // GET TARGET
-          if (options.draggable !== false) {
-            mouse.target = $daddy.find(jQclass).has(this);
-          } else {
-            mouse.target = $self;
-          }
-
-          // ADD CLONE
-          mouse.clone = mouse.target.clone();
-          mouse.target.css({ visibility: "hidden" }).addClass("active");
-          mouse.clone.addClass(cloneClass);
-          $daddy.append(mouse.clone);
-
-          // ADD PLACEHOLDER
-          var $placeholder = $("<div></div>");
-          mouse.placeholder = $placeholder;
-          mouse.placeholder.addClass(holderClass);
-          mouse.placeholder
-            .css({
-              top: mouse.target.offset().top - $daddy.offset().top,
-              left: mouse.target.offset().left - $daddy.offset().left,
-              width: mouse.target.outerWidth() - 10,
-              height: mouse.target.outerHeight() - 10,
-              lineHeight: mouse.target.height() - 18 + "px",
-              textAlign: "center",
-            })
-            .text(placeholder);
-
-          $daddy.append(mouse.placeholder);
-
-          // GET OFFSET FOR CLONE
-          var bLeft = Math.floor(parseFloat($daddy.css("border-left-width")));
-          var bTop = Math.floor(parseFloat($daddy.css("border-top-width")));
-          var difx =
-            mouse.x - mouse.target.offset().left + $daddy.offset().left + bLeft;
-          var dify =
-            mouse.y - mouse.target.offset().top + $daddy.offset().top + bTop;
-          mouse.cloneoffset.x = difx;
-          mouse.cloneoffset.y = dify;
-
-          // REMOVE THE CHILDREN DAD CLASS AND SET THE POSITION ON SCREEN
-          mouse.clone.removeClass(childrenClass).css({
-            position: "absolute",
-            top: mouse.y - mouse.cloneoffset.y,
-            left: mouse.x - mouse.cloneoffset.x,
-          });
-
-          // UNABLE THE TEXT SELECTION AND SET THE GRAB CURSOR
-          $("html,body").addClass("dad-noSelect");
-        }
-      });
-
-      $daddy.on("mouseenter touchenter", jQclass, function () {
-        dadUpdate($(this));
-      });
+    // Add placeholder
+    this.$placeholder = $("<div />");
+    this.$placeholder.css({
+      top: this.$target.offset().top - this.$container.offset().top,
+      left: this.$target.offset().left - this.$container.offset().left,
+      width: this.$target.outerWidth() - 10,
+      height: this.$target.outerHeight() - 10,
+      lineHeight: this.$target.height() - 18 + "px",
+      textAlign: "center",
     });
 
-    return this;
+    this.$container.append(mouse.placeholder);
+
+    // Update clone offset
+    var borderLeft = Math.floor(
+      parseFloat(this.$container.css("border-left-width"))
+    );
+    var borderTop = Math.floor(
+      parseFloat(this.$container.css("border-top-width"))
+    );
+
+    this.mouse.offsetX =
+      this.mouse.positionX +
+      this.$target.offset().left +
+      this.$container.offset().left +
+      borderLeft;
+
+    this.mouse.offsetY =
+      this.mouse.positionY -
+      this.$target.offset().top +
+      this.$container.offset().top +
+      borderTop;
+
+    // Add styles
+    this.$target.css("visibility", "hidden"); //.add-class active
+    this.$clone.css("position", "absolute");
+
+    // Set clone position
+    this.updateClonePosition();
+
+    // Disable item selection
+    // TODO: add inline css
+    $("html, body").addClass("dad-noSelect");
+  };
+
+  /**
+   * Middle step, occurs on mousemove
+   */
+  Dad.prototype.update = function (e) {
+    // Check if it is touch
+    if (supportsTouch && e.type == "touchmove") {
+      var mouseTarget = document.elementFromPoint(ev.clientX, ev.clientY);
+      $(mouseTarget).trigger("touchenter"); // TODO: check if this is necessary
+      // Update mouse using touch event
+      this.mouse.update(e.originalEvent.touches[0]);
+    } else {
+      this.mouse.update(e);
+    }
+
+    // Ipdate clone position
+    this.updateClonePosition();
+  };
+
+  /**
+   * Final step, ocurrs on mouseup
+   */
+  Dad.prototype.end = function () {
+    if (!this.$target) return;
+
+    var $target = this.$target;
+    var $clone = this.$clone;
+    var $container = this.$container;
+
+    // Maybe we will use this in the future
+    //Math.floor(parseFloat($daddy.css('border-left-width')));
+    //Math.floor(parseFloat($daddy.css('border-top-width')));
+    if ($.contains($container[0], $target[0])) {
+      // Do transition from clone to target
+      $clone.animate(
+        {
+          top: $target.offset().top - $container.offset().top,
+          left: $target.offset().left - $container.offset().left,
+        },
+        300,
+        this.reset.bind(this)
+      );
+    } else {
+      $clone.fadeOut(300, this.reset.bind(this));
+    }
+
+    // updatePosition($daddy);
+
+    $("html, body").removeClass("dad-noSelect");
+  };
+
+  Dad.prototype.reset = function () {
+    // Reset target visibility
+    if (this.$target) this.$target.css("visibility", "visible");
+    // Remove elements if existent
+    if (this.$clone) this.$clone.remove();
+    if (this.$placeholder) this.$placeholder.remove();
+
+    // Reset variables
+    this.$target = null;
+    this.$clone = null;
+    this.$placeholder = null;
+  };
+
+  Dad.prototype.updateClonePosition = function () {
+    if (!this.$clone) return;
+
+    var mouse = this.mouse;
+
+    this.$clone.css({
+      top: mouse.positionY - mouse.offsetY,
+      left: mouse.positionX - mouse.offsetX,
+    });
+  };
+
+  /**
+   * Dad update placeholder position by
+   * checking the current placeholder position
+   */
+  Dad.prototype.updatePlaceholderPosition = function () {
+    if (!this.$target) return;
+
+    var $container = this.$container;
+    var $target = this.target;
+    var $origin = $('<span style="display:none"></span>');
+    var $newplace = $('<span style="display:none"></span>');
+
+    // TODO: check Who puts this active class
+    if (obj.prevAll().hasClass("active")) {
+      obj.after($newplace);
+    } else {
+      obj.before($newplace);
+    }
+
+    mouse.target.before($origin);
+    $newplace.before(mouse.target);
+
+    mouse.placeholder.css({
+      top: $target.offset().top - $container.offset().top,
+      left: $target.offset().left - $container.offset().left,
+      width: $target.outerWidth() - 10,
+      height: $target.outerHeight() - 10,
+    });
+
+    $origin.remove();
+    $newplace.remove();
+  };
+
+  Dad.prototype.activate = function () {
+    // $daddy.addClass("dad-active"); do we really need to toggle this class?
+    this.active = false;
+  };
+
+  Dad.prototype.deactivate = function () {
+    this.active = false;
+  };
+
+  $.fn.dad = function (options) {
+    var options = $.extend(
+      {
+        target: ">div",
+        placeholder: "",
+        active: true,
+      },
+      options
+    );
+
+    $(this).each(function () {
+      this.dad = new Dad(this, options);
+    });
+
+    return {
+      addDropzone: function () {},
+    };
   };
 })(jQuery);
